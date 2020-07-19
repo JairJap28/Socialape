fc3561d477492fedc277f87359a5b06c0a771d73
Jair Jimenez Munevar <jairmunevar@gmail.com>
2020-07-18 20:14:52 -0500

    Structure commit, change folder names

---
+++ FrontEnd-SocialWorks/src/pages/home.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+
+// Components
+import Scream from '../components/Scream';
+import Profile from '../components/Profile';
+
+class home extends Component {
+    state = {
+        screams: null
+    };
+
+    componentDidMount() {
+        axios.get('screams').then(res => {
+            this.setState({
+                screams: res.data
+            });
+        }).catch(err => console.log(err));
+    }
+
+    render() {
+        let recentScreamsMarkup = this.state.screams ? (
+            this.state.screams.map(scream => (
+                <Scream key={scream.screamId} scream={scream} />)
+            )
+        ) : (
+            <p>Loading....</p>
+        );
+        return (
+            <Grid container spacing={5} >
+                <Grid item sm={8} xs={12}>
+                    { recentScreamsMarkup }
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <Profile />
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default home;
