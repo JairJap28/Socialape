5f1077f6930d5a20d86c532f505df0632ce1105b
Jair Jimenez Munevar <jairmunevar@gmail.com>
2020-07-24 21:02:21 -0500

    Create useer page

--- FrontEnd-SocialWorks/src/pages/login.js
+++ FrontEnd-SocialWorks/src/pages/login.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import AppIcon from '../images/icon.png';
 
 // MUI Stuff
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
